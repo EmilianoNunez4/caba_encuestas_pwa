3d60cc340f8f8c5662f2a5993ee5881462fd2854
EmilianoNunez4 <emilianonunezz44@gmail.com>
2025-07-15 18:49:25 -0300

    commit 11 nuevo

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,20 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDktnfDVAwTjdLgApgx6jOiph8fCVVQsjY",
   authDomain: "caba-encuestas.firebaseapp.com",
   projectId: "caba-encuestas",
-  storageBucket: "caba-encuestas.firebasestorage.app",
+  storageBucket: "caba-encuestas.appspot.com",
   messagingSenderId: "938428364499",
   appId: "1:938428364499:web:200378af937b11af178626"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const messaging = getMessaging(app);
 const googleProvider = new GoogleAuthProvider();
 
-export { auth, signInWithEmailAndPassword, signInWithPopup, googleProvider };
-
+export { auth, messaging, signInWithEmailAndPassword, signInWithPopup, googleProvider };
